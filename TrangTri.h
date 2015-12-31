7385bbccae7a2eb8847ff9d6844b4c3108c53575
theiron97 <theiron1997@gmail.com>
2016-01-01 02:35:31 +0700

    Create TrangTri.h

---
+++ TrangTri.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define YMAX 30
+#define YMIN 5
+#define XMIN 1
+#define XMAX 80
+
+#define XLENGTH 80
+#define CHR 219
+
+class TrangTri
+{
+public:
+	TrangTri();
+	~TrangTri();
+	void ve();
+};
+
